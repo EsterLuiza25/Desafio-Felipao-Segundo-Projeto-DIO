974dd2650e042ed76435bf232e77a3e0c04ec62e
Ester Luiza <luiza.campossouza.25@gmail.com>
2024-09-29 23:56:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function nivelPartidas(vitorias) {
+    
+    if (vitorias < 10) {
+        return "Ferro";
+    } else if (vitorias <= 20) {
+        return "Bronze";
+    } else if (vitorias <= 50) {
+        return "Prata";
+    } else if (vitorias <= 80) {
+        return "Ouro";
+    } else if (vitorias <= 90) {
+        return "Diamante";
+    } else if (vitorias <= 100) {
+        return "Lendário";
+    } else {
+        return "Imortal"; 
+    }
+}
+
+function nivelRanqueadas(vitorias, derrotas) {
+   
+    let saldo = vitorias - derrotas;
+    let nivel = nivelPartidas(vitorias);
+   
+    return `O Herói tem um saldo de ${saldo} vitórias e está no nível ${nivel}!`;
+}
+
+console.log(nivelRanqueadas(85, 30));
